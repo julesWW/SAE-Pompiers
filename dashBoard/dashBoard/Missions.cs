0c49949ecd4b4d8108ebbc5d5b8d3a373c128083
lohan <lohan.marchand@etu.unistra..fr>
2025-05-13 18:21:18 +0200

    Création de dashBoard (le tableau de bord)

---
+++ dashBoard/dashBoard/Missions.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Pinpon;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Mission
+{
+    public partial class uscMissions : UserControl
+    {
+        int id = 7;
+      //  SQLiteConnection connec;
+        string mission;
+        public uscMissions()
+        {
+            /*InitializeComponent();
+            connec = Connexion.Connec;
+
+            DataTable dt = connec.GetSchema("Tables");
+           // string liste = "";
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                string nomTable = dt.Rows[i][2].ToString();
+                string req = "select * from " + nomTable;
+                SQLiteCommand cmd = new SQLiteCommand(req, connec);
+                SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
+                da.Fill(MesDatas.DsGlobal, nomTable);
+                 // liste = liste + nomTable + "\n";
+            }
+            //  MessageBox.Show(liste);
+            Connexion.FermerConnexion();*/
+
+        }
+        public uscMissions(int idMission)
+        {
+            id=idMission;
+            InitializeComponent();
+        }
+
+        private void uscMissions_Load(object sender, EventArgs e)
+        {
+            lblIdMission.Text = "id mission: " + id;
+
+            foreach (DataRow r in MesDatas.DsGlobal.Tables["Mission"].Rows)
+            {
+                if (Convert.ToInt16(r[0]) == id)
+                {
+                    //Motif
+                    lblMotif.Text = "→ " + r["motifAppel"].ToString();
+                    mission = r["motifAppel"].ToString();
+                    //dateDepart
+                    string[] datesHeures = r["dateHeureDepart"].ToString().Split(' ');
+                    string[] dates = datesHeures[0].Split('-');
+                    lblDebut.Text = ("Début le " + dates[1] + "/" + dates[2] + "/" + dates[0] + " à " + datesHeures[1]);
+                    //caserne
+                    foreach (DataRow r2 in MesDatas.DsGlobal.Tables["Caserne"].Rows)
+                    {
+                        if (Convert.ToInt16(r2[0]) == Convert.ToInt16(r["idCaserne"]))
+                        {
+                            lblCaserne.Text = r2["nom"].ToString();
+                        }
+                    }
+                    //Nature Sinistre (catégorie)
+                    foreach (DataRow r2 in MesDatas.DsGlobal.Tables["NatureSinistre"].Rows)
+                    {
+                        if (Convert.ToInt16(r2[0]) == Convert.ToInt16(r["idNatureSinistre"]))
+                        {
+                            lblCategorie.Text = r2["libelle"].ToString();
+                        }
+                    }
+                    //Compte Rendu
+                    lblCompteRendu.Text = r["compteRendu"].ToString();
+
+                    // if pas fini
+                    if (Convert.ToInt16(r["terminee"]) == 0)
+                    {
+                        lblCompteRendu.Visible = false;
+                        lblFin.Visible = false;
+                        //Image
+                        var rm = dashBoard.Properties.Resources.ResourceManager;
+                        pboAlert.BackgroundImage = rm.GetObject("Alert") as Image;
+                    }
+                    else
+                    {
+                        btnCloture.Enabled = false;
+                        lblCompteRendu.Visible = true;
+                        lblFin.Visible = true;
+                        //Image
+                        var rm = dashBoard.Properties.Resources.ResourceManager;
+                        pboAlert.BackgroundImage = rm.GetObject("AlertOff") as Image;
+                        btnCloture.BackgroundImage = rm.GetObject("FireManThumbsUpOff") as Image;
+
+
+                        //dateRetour
+                        string[] datesHeuresRetour = r["dateHeureRetour"].ToString().Split(' ');
+                        string[] datesRetour = datesHeuresRetour[0].Split('-');
+                        lblFin.Text = ("Fin le " + datesRetour[1] + "/" + datesRetour[2] + "/" + datesRetour[0] + " à " + datesHeuresRetour[1]);
+                    }
+                }
+            }
+        }
+        private void btnCloture_Click(object sender, EventArgs e)
+        {
+            frmCR frmCR = new frmCR(mission, id);
+            if (frmCR.ShowDialog() == DialogResult.OK)
+            {
+                uscMissions_Load(sender, e);
+            }
+        }
+
+        private void btnPDF_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Création du PDF!!!");
+        }
+    }
+}
---
+++ dashBoard/dashBoard/dashBoard.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace dashBoard
+{
+    partial class uscdashBoard
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDashBoard = new System.Windows.Forms.Label();
+            this.btnNouvelle = new System.Windows.Forms.Button();
+            this.chkEnCours = new System.Windows.Forms.CheckBox();
+            this.flpMissions = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // lblDashBoard
+            // 
+            this.lblDashBoard.AutoSize = true;
+            this.lblDashBoard.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDashBoard.ForeColor = System.Drawing.Color.White;
+            this.lblDashBoard.Location = new System.Drawing.Point(272, 17);
+            this.lblDashBoard.Name = "lblDashBoard";
+            this.lblDashBoard.Size = new System.Drawing.Size(207, 29);
+            this.lblDashBoard.TabIndex = 0;
+            this.lblDashBoard.Text = "Tableau de bord";
+            // 
+            // btnNouvelle
+            // 
+            this.btnNouvelle.BackColor = System.Drawing.Color.LightCoral;
+            this.btnNouvelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNouvelle.Location = new System.Drawing.Point(615, 17);
+            this.btnNouvelle.Name = "btnNouvelle";
+            this.btnNouvelle.Size = new System.Drawing.Size(137, 64);
+            this.btnNouvelle.TabIndex = 1;
+            this.btnNouvelle.Text = "Nouvelle mission";
+            this.btnNouvelle.UseVisualStyleBackColor = false;
+            // 
+            // chkEnCours
+            // 
+            this.chkEnCours.AutoSize = true;
+            this.chkEnCours.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkEnCours.ForeColor = System.Drawing.Color.White;
+            this.chkEnCours.Location = new System.Drawing.Point(41, 36);
+            this.chkEnCours.Name = "chkEnCours";
+            this.chkEnCours.Size = new System.Drawing.Size(111, 29);
+            this.chkEnCours.TabIndex = 2;
+            this.chkEnCours.Text = "En cours";
+            this.chkEnCours.UseVisualStyleBackColor = true;
+            this.chkEnCours.CheckedChanged += new System.EventHandler(this.chkEnCours_CheckedChanged);
+            // 
+            // flpMissions
+            // 
+            this.flpMissions.AutoScroll = true;
+            this.flpMissions.Location = new System.Drawing.Point(4, 85);
+            this.flpMissions.Name = "flpMissions";
+            this.flpMissions.Size = new System.Drawing.Size(794, 505);
+            this.flpMissions.TabIndex = 3;
+            // 
+            // uscdashBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Brown;
+            this.Controls.Add(this.flpMissions);
+            this.Controls.Add(this.chkEnCours);
+            this.Controls.Add(this.btnNouvelle);
+            this.Controls.Add(this.lblDashBoard);
+            this.Name = "uscdashBoard";
+            this.Size = new System.Drawing.Size(800, 600);
+            this.Load += new System.EventHandler(this.uscdashBoard_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDashBoard;
+        private System.Windows.Forms.Button btnNouvelle;
+        private System.Windows.Forms.CheckBox chkEnCours;
+        private System.Windows.Forms.FlowLayoutPanel flpMissions;
+    }
+}
