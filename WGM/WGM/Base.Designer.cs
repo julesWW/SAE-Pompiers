3089af6a66ba6b56ff22b8d056b17ef6b0d5b182
lohan <lohan.marchand@etu.unistra..fr>
2025-06-04 23:33:12 +0200

    ajout d'icone d'onglets

--- WGM/WGM/Base.Designer.cs
+++ WGM/WGM/Base.Designer.cs
@@ -30,18 +30,26 @@
         {
             this.btnQuitter = new System.Windows.Forms.Button();
             this.grbMenu = new System.Windows.Forms.GroupBox();
+            this.pboStats = new System.Windows.Forms.PictureBox();
+            this.pboPersonnel = new System.Windows.Forms.PictureBox();
+            this.pboEngin = new System.Windows.Forms.PictureBox();
+            this.pboDashboard = new System.Windows.Forms.PictureBox();
             this.btnPersonnel = new System.Windows.Forms.Button();
             this.btnStatistiques = new System.Windows.Forms.Button();
             this.btnEngins = new System.Windows.Forms.Button();
             this.btnTableauDeBord = new System.Windows.Forms.Button();
             this.pnlPage = new System.Windows.Forms.Panel();
             this.grbMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pboStats)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboPersonnel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboEngin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboDashboard)).BeginInit();
             this.SuspendLayout();
             // 
             // btnQuitter
             // 
-            this.btnQuitter.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-            | System.Windows.Forms.AnchorStyles.Left)
+            this.btnQuitter.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnQuitter.BackColor = System.Drawing.Color.WhiteSmoke;
             this.btnQuitter.BackgroundImage = global::WGM.Properties.Resources.quit;
@@ -60,6 +68,10 @@
             // grbMenu
             // 
             this.grbMenu.BackColor = System.Drawing.Color.Brown;
+            this.grbMenu.Controls.Add(this.pboStats);
+            this.grbMenu.Controls.Add(this.pboPersonnel);
+            this.grbMenu.Controls.Add(this.pboEngin);
+            this.grbMenu.Controls.Add(this.pboDashboard);
             this.grbMenu.Controls.Add(this.btnPersonnel);
             this.grbMenu.Controls.Add(this.btnStatistiques);
             this.grbMenu.Controls.Add(this.btnEngins);
@@ -71,12 +83,52 @@
             this.grbMenu.TabIndex = 1;
             this.grbMenu.TabStop = false;
             // 
+            // pboStats
+            // 
+            this.pboStats.BackgroundImage = global::WGM.Properties.Resources.statistical;
+            this.pboStats.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pboStats.Location = new System.Drawing.Point(11, 235);
+            this.pboStats.Name = "pboStats";
+            this.pboStats.Size = new System.Drawing.Size(50, 50);
+            this.pboStats.TabIndex = 8;
+            this.pboStats.TabStop = false;
+            // 
+            // pboPersonnel
+            // 
+            this.pboPersonnel.BackgroundImage = global::WGM.Properties.Resources.firefighter2;
+            this.pboPersonnel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pboPersonnel.Location = new System.Drawing.Point(11, 179);
+            this.pboPersonnel.Name = "pboPersonnel";
+            this.pboPersonnel.Size = new System.Drawing.Size(50, 50);
+            this.pboPersonnel.TabIndex = 7;
+            this.pboPersonnel.TabStop = false;
+            // 
+            // pboEngin
+            // 
+            this.pboEngin.BackgroundImage = global::WGM.Properties.Resources.firetruck;
+            this.pboEngin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pboEngin.Location = new System.Drawing.Point(11, 123);
+            this.pboEngin.Name = "pboEngin";
+            this.pboEngin.Size = new System.Drawing.Size(50, 50);
+            this.pboEngin.TabIndex = 6;
+            this.pboEngin.TabStop = false;
+            // 
+            // pboDashboard
+            // 
+            this.pboDashboard.BackgroundImage = global::WGM.Properties.Resources.tasks;
+            this.pboDashboard.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pboDashboard.Location = new System.Drawing.Point(11, 67);
+            this.pboDashboard.Name = "pboDashboard";
+            this.pboDashboard.Size = new System.Drawing.Size(50, 50);
+            this.pboDashboard.TabIndex = 5;
+            this.pboDashboard.TabStop = false;
+            // 
             // btnPersonnel
             // 
-            this.btnPersonnel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-            | System.Windows.Forms.AnchorStyles.Left)
+            this.btnPersonnel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnPersonnel.Location = new System.Drawing.Point(57, 159);
+            this.btnPersonnel.Location = new System.Drawing.Point(71, 182);
             this.btnPersonnel.Name = "btnPersonnel";
             this.btnPersonnel.Size = new System.Drawing.Size(148, 42);
             this.btnPersonnel.TabIndex = 4;
@@ -86,11 +138,11 @@
             // 
             // btnStatistiques
             // 
-            this.btnStatistiques.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-            | System.Windows.Forms.AnchorStyles.Left)
+            this.btnStatistiques.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnStatistiques.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnStatistiques.Location = new System.Drawing.Point(57, 207);
+            this.btnStatistiques.Location = new System.Drawing.Point(71, 238);
             this.btnStatistiques.Name = "btnStatistiques";
             this.btnStatistiques.Size = new System.Drawing.Size(148, 42);
             this.btnStatistiques.TabIndex = 3;
@@ -99,10 +151,10 @@
             // 
             // btnEngins
             // 
-            this.btnEngins.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-            | System.Windows.Forms.AnchorStyles.Left)
+            this.btnEngins.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnEngins.Location = new System.Drawing.Point(57, 111);
+            this.btnEngins.Location = new System.Drawing.Point(71, 126);
             this.btnEngins.Name = "btnEngins";
             this.btnEngins.Size = new System.Drawing.Size(148, 42);
             this.btnEngins.TabIndex = 2;
@@ -112,12 +164,12 @@
             // 
             // btnTableauDeBord
             // 
-            this.btnTableauDeBord.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-            | System.Windows.Forms.AnchorStyles.Left)
+            this.btnTableauDeBord.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnTableauDeBord.BackColor = System.Drawing.Color.White;
             this.btnTableauDeBord.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnTableauDeBord.Location = new System.Drawing.Point(57, 63);
+            this.btnTableauDeBord.Location = new System.Drawing.Point(71, 70);
             this.btnTableauDeBord.Name = "btnTableauDeBord";
             this.btnTableauDeBord.Size = new System.Drawing.Size(148, 42);
             this.btnTableauDeBord.TabIndex = 1;
@@ -147,6 +199,10 @@
             this.Text = "Combattant du feu";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.grbMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pboStats)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboPersonnel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboEngin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboDashboard)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -160,6 +216,10 @@
         private System.Windows.Forms.Button btnStatistiques;
         private System.Windows.Forms.Button btnEngins;
         private System.Windows.Forms.Panel pnlPage;
+        private System.Windows.Forms.PictureBox pboStats;
+        private System.Windows.Forms.PictureBox pboPersonnel;
+        private System.Windows.Forms.PictureBox pboEngin;
+        private System.Windows.Forms.PictureBox pboDashboard;
     }
 }
 
